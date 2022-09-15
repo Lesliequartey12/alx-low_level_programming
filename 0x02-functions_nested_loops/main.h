2565ec78106d42b0f882fc62bf345da665cebccb
lesliequartey12 <lesliequartey12@gmail.com>
2022-09-14 20:58:25 -0700

    putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,9 +1,9 @@
 #ifndef HOLBERTON_H_INCLUDED
 #define HOLBERTON_H_INCLUDED
 
-int_putcahr(char);
+int_putchar(char);
 void print_alphabet(void);
-void print)alphabet_x10(void);
+void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
