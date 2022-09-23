fbade09f7fdb1f33cd7f5dcfaccefdc7d481ab32
lesliequartey12 <lesliequartey12@gmail.com>
2022-09-23 05:45:30 -0700

    putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 
 void print_alphabet(void);
-print_alphabet_x10(void);
+void print_alphabet_x10(void);
 int islower(int c);
 int _putchar(char c);
 int _isalpha(int c);
@@ -14,5 +14,6 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
+return: (0);
 
 #endif
